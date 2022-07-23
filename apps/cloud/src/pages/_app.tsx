a105bfaa6e9296844ac938230e769f100b26059f
Max Rose <max.rose366@gmail.com>
2022-07-23 17:36:36 +0100

    feat: nav and home pages

--- apps/cloud/src/pages/_app.tsx
+++ apps/cloud/src/pages/_app.tsx
@@ -1,14 +1,44 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { CssBaseline, GeistProvider } from '@geist-ui/core'
-import { withTRPC } from '@trpc/next'
-import { AppRouter } from 'src/backend/router'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { CssBaseline, GeistProvider, useTheme } from '@geist-ui/core';
+import { withTRPC } from '@trpc/next';
+import { AppRouter } from 'src/backend/router';
+import Nav from '@components/nav';
+import { useEffect, useState } from 'react';
+import { ConfigProvider } from '@context/config';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = useTheme();
+  const [themeType, setThemeType] = useState(theme.type);
+  
+  const themeChange = (theme: "light" | "dark") => {
+    window.localStorage.setItem("theme", theme);
+    setThemeType(theme);
+  }
+
+  useEffect(() => {
+    const theme = window.localStorage.getItem("theme");
+
+    if(theme && (theme === "light" || theme === "dark"))
+      themeChange(theme);
+    else
+      themeChange("dark");
+  }, []);
+
   return(
-    <GeistProvider>
+    <GeistProvider themeType={themeType}>
       <CssBaseline />
-      <Component {...pageProps} />
+      <ConfigProvider onThemeChange={themeChange}>
+        <Head>
+          <title>Cloud Secrets</title>
+          <link rel="icon" href="/favicon.png" />
+          <meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, viewport-fit=cover" />
+        </Head>
+
+        <Nav />
+        <Component {...pageProps} />
+      </ConfigProvider>
     </GeistProvider>
   )
 }
