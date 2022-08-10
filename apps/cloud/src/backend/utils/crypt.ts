d0c80aecbf8a6abba3ceff8639e08751d483b0fc
Max Rose <max.rose366@gmail.com>
2022-08-10 20:26:45 +0100

    feat: Encrypting data properly

--- apps/cloud/src/backend/utils/crypt.ts
+++ apps/cloud/src/backend/utils/crypt.ts
@@ -1,26 +1,7 @@
 import * as trpc from '@trpc/server';
 import { createCipheriv, createDecipheriv, randomBytes, scryptSync } from 'crypto';
 
-// 32 characters of padding to ensire we always have 1 block of padding
-const padding = '................................';
 const algorithm = 'aes-192-cbc';
-
-const getCipher = (encrypt = true, presetIv?: string) => {
-  const password = process.env.CONFIG_ENCRYPTION_SCRET;
-  const salt = process.env.CONFIG_ENCRYPTION_SALT;
-
-  if (!password || !salt) {
-    throw new trpc.TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Failed to create cipher' });
-  }
-
-  const iv = presetIv ? Buffer.from(presetIv, 'hex') : randomBytes(16);
-
-  const key = scryptSync(password, salt, 24);
-  return {
-    cipher: encrypt ? createCipheriv(algorithm, key, iv) : createDecipheriv(algorithm, key, iv),
-    iv: iv.toString('hex'),
-  };
-};
 
 const getEncryptionCipher = () => {
   const password = process.env.CONFIG_ENCRYPTION_SECRET;
@@ -41,7 +22,7 @@ const getEncryptionCipher = () => {
   };
 };
 
-const getDeEncryptionCipher = (salt: string, iv: string) => {
+const getDecryptionCipher = (salt: string, iv: string) => {
   const password = process.env.CONFIG_ENCRYPTION_SECRET;
 
   if (!password) {
@@ -54,10 +35,9 @@ const getDeEncryptionCipher = (salt: string, iv: string) => {
 };
 
 export const encryptConfig = (config: object): string => {
-  const strigified = `${JSON.stringify(config)}${padding}`;
   const { cipher, salt, iv } = getEncryptionCipher();
 
-  return `${cipher.update(strigified, 'utf-8', 'hex') as string}-${salt}-${iv}`;
+  return `${cipher.update(JSON.stringify(config), 'utf-8', 'hex') + cipher.final('hex')}-${salt}-${iv}`;
 };
 
 export const decryptConfig = (config: string): { [key: string]: string } => {
@@ -67,9 +47,9 @@ export const decryptConfig = (config: string): { [key: string]: string } => {
 
   const [cipherText, salt, iv] = config.split('-');
 
-  const cipher = getDeEncryptionCipher(salt, iv);
+  const cipher = getDecryptionCipher(salt, iv);
 
-  const result = (cipher.update(cipherText, 'hex', 'utf-8') as string).replace(/}\.+$/, '}');
+  const result = cipher.update(cipherText, 'hex', 'utf-8') + cipher.final('utf-8');
 
   return JSON.parse(result || '{}');
 };
