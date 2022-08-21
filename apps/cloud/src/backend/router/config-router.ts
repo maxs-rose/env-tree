57cd9846b8d2185d77d380b4c08a5ee89fb4f9f8
Max Rose <max.rose366@gmail.com>
2022-08-21 15:46:07 +0100

    chore: TODO comments

--- apps/cloud/src/backend/router/config-router.ts
+++ apps/cloud/src/backend/router/config-router.ts
@@ -30,6 +30,9 @@ export const configRouter = createRouter()
     resolve: ({ ctx, input }) =>
       firstValueFrom(linkedConfig$(ctx.user.id, input.projectId, input.targetConfig, input.configName)),
   })
+  // TODO: Add some mechanism to ensure that if multiple users are editing a config someone with an outdated version does not override the new one by accident
+  //  maybe good enough to add some kind of "version" into the get response for each config and reject requests with a mismatched version.
+  //  The version would have to be regenerated each time the config is updated.
   .mutation('update', {
     input: z.object({ projectId: z.string(), configId: z.string(), values: ZConfigValue }),
     resolve: ({ ctx, input }) =>
